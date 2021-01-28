a9672633e0761fb9b61f07ec1cc1ed9545626f17
Zenna Tavares <zennatavares@gmail.com>
2021-01-28 01:23:12 -0500

    car

---
+++ src/carcrash3dmodel.jl
@@ -0,0 +1,19 @@
+using OmegaCore
+using CarCrash
+using RayMarch
+
+function f(ω; n = [1, 3])
+  # Simulate the model
+  sim = CarCrash.model(ω)
+
+  # Render the scenes
+  # [RayMarch.]
+end
+
+function loss()
+
+end
+
+function query()
+
+end
