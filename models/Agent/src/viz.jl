4d3650610d241300c6800b5444355d3d3eede381
Zenna Tavares <zennatavares@gmail.com>
2021-02-03 19:07:50 -0500

    agent based model

---
+++ models/Agent/src/viz.jl
@@ -0,0 +1,70 @@
+using OpenStreetMapXPlot
+using Plots
+gr()
+
+ac(agent) = agent.infected ? :green : :black
+as(agent) = agent.infected ? 6 : 5
+
+function plotagents(model)
+    # Essentially a cut down version on plotabm
+    ids = model.scheduler(model)
+    colors = [ac(model[i]) for i in ids]
+    sizes = [as(model[i]) for i in ids]
+    markers = :circle
+    pos = [osm_map_coordinates(model[i], model) for i in ids]
+
+    scatter!(
+        pos;
+        markercolor = colors,
+        markersize = sizes,
+        markershapes = markers,
+        label = "",
+        markerstrokewidth = 0.5,
+        markerstrokecolor = :black,
+        markeralpha = 0.7,
+    )
+end
+
+function make_anim(; n = 200, path = "outbreak.gif")
+    model = initialise()
+
+    frames = @animate for i in 0:n
+        i > 0 && step!(model, agent_step!, 1)
+        plotmap(model.space.m)
+        plotagents(model)
+    end
+
+    gif(frames, path, fps = 15)
+end
+
+function make_anim_air(; n = 200, path = "outbreakair.gif")
+    model = initialise_air()
+
+    frames = @animate for i in 0:n
+        i > 0 && step!(model, agent_step_air!, model_step_air!, 1)
+        plotmap(model.space.m)
+        plotagents(model)
+    end
+
+    gif(frames, path, fps = 15)
+end
+
+function simulate(; n = 200)
+    model = initialise()
+
+    for i in 0:n
+        @show i
+        i > 0 && step!(model, agent_step!, 1)
+    end
+    model
+end
+
+function simulate_air(; n = 200)
+    model = initialise_air()
+
+    for i in 0:n
+        @show i
+        i > 0 && step!(model, agent_step_air!, model_step_air!, 1)
+    end
+    model
+end
--- models/CarCrash/src/queries.jl
+++ models/CarCrash/src/queries.jl
@@ -2,34 +2,48 @@
 # using Distributions
 
 import Omega
-using Omega.Prim: Normal, ~
+using Omega.Prim: Normal, ~, cond, replace
 const randsample = rand
+using Statistics: mean
 
 export prob, carjoint
 
 const μspeed = 3.0
 const accel = 1 ~ Normal(μspeed, 1)
 const car_init_vel = 2 ~ Normal(10.0, 2.0)
+const obs_x_pos = 4 ~ Normal(38.0, 0.01)
 # const CAR_INIT_VEL =10.0
 
 "Raandom variable over simulations"
 function simrv(ω) 
   decel = 9.0
   timestep = 0.1
+  x = obs_x_pos(ω)
+  x = 38
   simulate_scene(; ACCEL = accel(ω),
                    DECEL = decel,
                    CAR_INIT_VEL = car_init_vel(ω),
-                   timestep = timestep)
+                   timestep = timestep,
+                   OBSTRUCTION_POS = VecE2(x, -7.0))
 end
 
 crashed(ω) = check_collision(simrv(ω)[1])
 
-prob(x; n = 1000) = mean(randsample(x, n))
+animation_(ω) = animate_scene(simrv(ω)...)
+
+prob(x; n = 1000) = mean(rand(x, n; alg = Omega.RejectionSample))
 
 function run_cf()
-  cf_crash = (crashed |ᵈ (accel => 1.0)) |ᶜ crashed
+  # cf_crash = (crashed |ᵈ (accel => 1.0)) |ᶜ crashed
+  crashed_ = 3 ~ crashed
+  a = 0.01
+  cf_crash = cond(replace(crashed_, accel => a), crashed_)
   println("Given that driver crashed, the probability they would have crashed
-  had their acceleration been 1.0 ", prob(cf_crash))
+  had their acceleration been $a ", prob(cf_crash))
+
+  cf_crash = cond(replace(crashed_, obs_x_pos => 34), crashed_)
+  println("Given that driver crashed, the probability they would have crashed
+  had their the object moved", prob(cf_crash))
 end
 
 # carjoint = @joint(accel, car_init_vel, crashed, sim)
